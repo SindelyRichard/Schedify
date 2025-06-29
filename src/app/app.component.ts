06311baba33850212ae3911e9506ee04891b8eb2
SindelyRichard <rsindely@gmail.com>
2025-06-29 16:02:44 +0200

    fix:imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WelcomeComponent } from '../components/welcome/welcome.component';
 import {MatButtonModule} from '@angular/material/button'; 
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomeComponent,MatButtonModule,MatIconModule],
+  imports: [RouterOutlet,MatButtonModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from '../welcome/welcome.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, WelcomeComponent,MatButtonModule,MatIconModule],
+  imports: [ WelcomeComponent,MatButtonModule,MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
